571988e13b0b1122832aafc79de6d3631d6e4b12
Aayush Gupta <guptaaayush432@gmail.com>
2023-02-15 13:39:49 +0530

    Added icy-nft-hooks

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import "../styles/globals.css";
+import { IcyProvider } from "@quicknode/icy-nft-hooks";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <IcyProvider apiKey={QUICKNODE_NFT_API_KEY}>
+      <Component {...pageProps} />
+    </IcyProvider>
+  );
 }
