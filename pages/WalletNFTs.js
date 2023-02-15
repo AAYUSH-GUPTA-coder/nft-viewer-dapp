d5f4177d40146731730f4ebc0d81e86cfc9fd8ba
Aayush Gupta <guptaaayush432@gmail.com>
2023-02-15 23:36:15 +0530

    Added ENS

--- pages/WalletNFTs.js
+++ pages/WalletNFTs.js
@@ -2,17 +2,28 @@ import { useWalletNFTs } from "@quicknode/icy-nft-hooks";
 import { useState } from "react";
 
 function WalletNFTs() {
-  const [ensName, setEnsName] = useState("");
+  const [ensName, setEnsName] = useState("vitalik.eth");
+  const [address, setAddress] = useState("");
   const [cursor, setCursor] = useState("");
   const { nfts, isSearchValid, pageInfo } = useWalletNFTs({
+    address,
     ensName,
-    first: 10,
+    first: 12,
     after: cursor,
   });
 
   return (
-    <div className="WalletNFT bg-black text-white">
-      <div className="w-full h-full flex flex-row justify-evenly items-center">
+    <div className="p-10 flex flex-col items-center">
+      <h1 className='text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-6xl mb-4"'>
+        NFT Viewer
+      </h1>
+      <div className="flex-left flex-col mt-4">
+        <label
+          className="text-zinc-700 text-2xl font-extrabold"
+          htmlFor="wallet-address"
+        >
+          &nbsp; Wallet address: &nbsp;
+        </label>
         <div className="search">
           <input
             type="text"
@@ -25,29 +36,33 @@ function WalletNFTs() {
           />
         </div>
       </div>
-      {nfts.map((nft) => {
-        const contract = nft.contract;
-        const imageUrl = nft.images.find((i) => !!i.url)?.url;
 
-        return (
-          <div
-            className="bg-gray-800 w-1/5 h-fit rounded-md"
-            key={`${nft.tokenId}${nft.contract.address}`}
-          >
-            {imageUrl && (
-              <div className="p-3 w-full h-auto">
-                <img src={imageUrl} alt="nft awesome" />
+      <div className="grid grid-cols-4 mt-8 gap-4">
+        {nfts.map((nft) => {
+          const contract = nft.contract;
+          const imageUrl = nft.images.find((i) => !!i.url)?.url;
+
+          return (
+            <div
+              className="flex flex-col rounded border p-4"
+              key={`${nft.tokenId}${nft.contract.address}`}
+            >
+              {imageUrl && (
+                <div className="w-[200px] h-[200px] rounded shadow">
+                  <img src={imageUrl} alt="nft awesome" />
+                </div>
+              )}
+              <div>
+                <h1>{contract.name}</h1>
+                <h2>
+                  {contract.symbol}#{nft.tokenId}
+                </h2>
               </div>
-            )}
-            <div>
-              <h1>{contract.name}</h1>
-              <h2>
-                {contract.symbol}#{nft.tokenId}
-              </h2>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+
 
       {pageInfo?.hasNextPage && (
         <div
